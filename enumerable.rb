3278a9dd4d9a8bda5874f92aacf7f93c839f78b1
fithamlak <justiceofgod01@gmail.com>
2023-04-14 20:07:30 +0300

    add lists and checkfunctionalities

--- enumerable.rb
+++ enumerable.rb
@@ -10,4 +10,11 @@ class MyList
     @list.each(&block)
   end
 end
+list = MyList.new(1, 2, 3, 4)
+puts(list.all? { |e| e < 5 })
+puts(list.all? { |e| e > 5 })
 
+puts(list.any? { |e| e == 2 })
+puts(list.any? { |e| e == 5 })
+
+puts(list.filter(&:even?))
